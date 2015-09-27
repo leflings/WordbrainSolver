e049c7268ad20a98efff363154aef8d3572e5cb6
Flemming Madsen <mail@leflings.dk>
2015-09-27 10:35:09 +0200

    Add copy and trickle down function for the Board

--- WordbrainSolver/Board.fs
+++ WordbrainSolver/Board.fs
@@ -16,11 +16,30 @@ let isValidPosition board (posx,posy) =
     let lx, ly = Array2D.length1 board, Array2D.length2 board
     not (posy < 0 || posx < 0 || posy >= ly || posx >= lx)
 
-//let canMove board pos dir = move pos dir |> isValidPosition board
-//let swap b p1 p2 =
-//    let x = p1 ||> Array2D.get b
-//    (Array2D.set b <|| p1) <| (Array2D.get b <|| p2)
-//    (Array2D.set b <|| p2) x
-//    b
-//
-//let validMoves board pos = moves |> List.filter (move pos >> isValidPosition board) 
+let swap b p1 p2 =
+    let x = p1 ||> Array2D.get b
+    (Array2D.set b <|| p1) <| (Array2D.get b <|| p2)
+    (Array2D.set b <|| p2) x
+    b
+
+
+
+let private trickleDownColumns board =
+    let shrinkColumn col =
+        let n = Array.length col
+        let validChars =  col |> Array.filter ((<>) ' ')
+        let count = Array.length validChars
+        let startIndex = n - count
+        let newArray = Array.create n ' '
+        Array.blit validChars 0 newArray startIndex count
+        newArray
+    let n = Array2D.length2 board
+    for i in 0 .. n - 1 do
+        let col = board.[*,i]
+        board.[*,i] <- col |> shrinkColumn
+    board
+
+let copyBoardWithout board taken =
+    let newBoard = Array2D.copy board
+    taken |> List.iter (fun x -> set newBoard x ' ')
+    newBoard |> trickleDownColumns
